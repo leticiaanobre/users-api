0917f472c3142d6dd26a5b7bfdbfb6d131a97e89
leticia nobre <leticiavidal2910@gmail.com>
2024-12-02 17:47:53 -0300

    feat: database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import {ConfigModule} from '@nestjs/config';
+import {TypeOrmModule} from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      database: process.env.DB_DATABASE,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      synchronize: true,
+      logging: false,
+      migrations: [__dirname + '/database/migrations/*{.js,.ts}'],
+      entities: [__dirname + '/**/*.entity{.js,.ts}'],
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
