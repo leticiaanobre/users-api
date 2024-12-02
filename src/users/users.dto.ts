4f31565125c6fc233f5539e52f941345db8cf7d7
leticia nobre <leticiavidal2910@gmail.com>
2024-12-02 18:11:59 -0300

    feat: created usersdto and adjusted user service

---
+++ src/users/users.dto.ts
@@ -0,0 +1,8 @@
+export class UsersDto {
+    readonly id?: string;
+    readonly name: string;
+    readonly email: string;
+    readonly password: string;
+    readonly createAt?: string;
+    readonly updatedAt?: string;
+}
