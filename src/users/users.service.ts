8531a461dca8e5d2b342ebc65a55daf5f1d1d34a
leticia nobre <leticiavidal2910@gmail.com>
2024-12-02 18:41:23 -0300

    feat: added error treatment

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Users } from './user.entity';
@@ -13,6 +13,8 @@ export class UsersService {
 
     async findAllUsers(): Promise<Users[]> {
         const users = await this.usersRepository.find()
+
+        if(users.length ===0) throw new HttpException('Users not found!', HttpStatus.NOT_FOUND)
         return users;
     }
 
