6da3cd3e27b379155abab6a39e39f3e01bba6a81
leticia nobre <leticiavidal2910@gmail.com>
2024-12-02 18:22:17 -0300

    fix: importes entity

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Users } from './user.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Users]) //  avisa que está usando uma entidade de nome 'Users'
+  ],
   controllers: [UsersController],
   providers: [UsersService]
 })
